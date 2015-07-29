c679f3f589987cd28a81238b43e308e32a8283e2
Choon-Chern Lim <limc@mayo.edu>
2015-07-29 16:00:58 -0500

    Messing with Materialize

--- choonchernlim-archetype-webapp-webapp/choonchernlim-archetype-webapp-webapp-war/src/main/frontend/app/app.js
+++ choonchernlim-archetype-webapp-webapp/choonchernlim-archetype-webapp-webapp-war/src/main/frontend/app/app.js
@@ -6,15 +6,15 @@ var moment = require( 'moment' );
 var Promise = require( 'bluebird' );
 
 $( function () {
-    var $body = $( 'body' );
-
-    $body.append( '<h1>Testing jQuery, LoDash, Moment</h1>' );
-    $body.append( '<p>Moment: ' + moment().format() + '</p>' );
-
-    $body.append( '<p>' + Promise + '</p>' );
-
-    _.each( [1, 2], function ( num ) {
-        $body.append( '<p>LoDash: ' + num + '</p>' );
-    } );
+    //var $body = $( 'body' );
+    //
+    //$body.append( '<h1>Testing jQuery, LoDash, Moment</h1>' );
+    //$body.append( '<p>Moment: ' + moment().format() + '</p>' );
+    //
+    //$body.append( '<p>' + Promise + '</p>' );
+    //
+    //_.each( [1, 2], function ( num ) {
+    //    $body.append( '<p>LoDash: ' + num + '</p>' );
+    //} );
 } );
 
