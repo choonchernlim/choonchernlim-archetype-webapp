0b1713a61a39dac25f1fe6205b2c9a35a58768a5
Choon-Chern Lim <meteoric@rocketmail.com>
2015-08-02 08:49:09 -0500

    Fixed scroll spy select

--- choonchernlim-archetype-webapp-webapp/choonchernlim-archetype-webapp-webapp-war/src/main/frontend/src/js/app.js
+++ choonchernlim-archetype-webapp-webapp/choonchernlim-archetype-webapp-webapp-war/src/main/frontend/src/js/app.js
@@ -9,8 +9,15 @@ require( 'bootstrap' );
 // getting scroll-spy content to align properly on menu click
 var offset = 100;
 $( '#home-left-navbar' ).find( 'li a' ).click( function ( e ) {
+    var $a = $( this );
+    var $li = $( this ).parent();
+
     e.preventDefault();
 
-    $( $( this ).attr( 'href' ) )[0].scrollIntoView();
+    $( $a.attr( 'href' ) )[0].scrollIntoView();
     scrollBy( 0, -offset );
+
+    $a.blur();
+
+    $li.addClass( 'active' ).siblings().removeClass( 'active' );
 } );
