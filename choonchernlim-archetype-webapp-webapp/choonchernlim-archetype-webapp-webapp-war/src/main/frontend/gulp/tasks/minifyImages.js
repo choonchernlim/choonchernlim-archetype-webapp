05958ce9aaf700dc1560c3f252824d67bd8becbf
Mike <meteoric@rocketmail.com>
2015-08-05 17:10:23 -0500

    Retry gulp-imagemin and node 0.12.... duh!

--- choonchernlim-archetype-webapp-webapp/choonchernlim-archetype-webapp-webapp-war/src/main/frontend/gulp/tasks/minifyImages.js
+++ choonchernlim-archetype-webapp-webapp/choonchernlim-archetype-webapp-webapp-war/src/main/frontend/gulp/tasks/minifyImages.js
@@ -1,13 +1,13 @@
 var changed = require( 'gulp-changed' );
 var gulp = require( 'gulp' );
-var imagemop = require( 'gulp-image-optimization' );
+var imagemin = require('gulp-imagemin');
 var config = require( '../config' ).images;
 var browserSync = require( 'browser-sync' );
 
 gulp.task( 'minifyImages', function () {
     return gulp.src( config.src )
         .pipe( changed( config.dest ) ) // Ignore unchanged files
-        .pipe( imagemop() ) // Optimize
+        .pipe( imagemin() ) // Optimize
         .pipe( gulp.dest( config.dest ) )
         .pipe( browserSync.reload( {stream : true} ) );
 } );
