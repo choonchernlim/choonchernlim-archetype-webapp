7ed717a7e70ed88b7f9a766080dfbf193bce1149
Choon-Chern Lim <meteoric@rocketmail.com>
2016-04-15 21:01:03 -0500

    todo

--- choonchernlim-archetype-webapp-webapp/choonchernlim-archetype-webapp-webapp-war/src/main/java/com/github/choonchernlim/choonchernlimArchetypeWebapp/controller/IndexController.java
+++ choonchernlim-archetype-webapp-webapp/choonchernlim-archetype-webapp-webapp-war/src/main/java/com/github/choonchernlim/choonchernlimArchetypeWebapp/controller/IndexController.java
@@ -3,7 +3,7 @@ package com.github.choonchernlim.choonchernlimArchetypeWebapp.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-// TODO client side will serve the entry page
+// TODO configure a catch all (minus /api/*) to redirect user to index.html if user bookmark the single-page app links.
 //@Controller
 //@RequestMapping(value = "/")
 public final class IndexController {
