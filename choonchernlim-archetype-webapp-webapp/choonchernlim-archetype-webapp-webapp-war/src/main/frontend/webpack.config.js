9270e93cf4abde469d89b3bd1435b374bae99039
Choon-Chern Lim <meteoric@rocketmail.com>
2016-04-15 20:53:15 -0500

    fixed react router with context root

--- choonchernlim-archetype-webapp-webapp/choonchernlim-archetype-webapp-webapp-war/src/main/frontend/webpack.config.js
+++ choonchernlim-archetype-webapp-webapp/choonchernlim-archetype-webapp-webapp-war/src/main/frontend/webpack.config.js
@@ -25,11 +25,11 @@ module.exports = Object.assign({}, baseConfig, {
     // "It looks like you're using a minified copy of the development build of React.
     // When deploying React apps to production, make sure to use the production build
     // which skips development warnings and is faster."
-    // new webpack.DefinePlugin({
-    //   'process.env': {
-    //     NODE_ENV: JSON.stringify('production')
-    //   }
-    // }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
 
     // Prevents the inclusion of duplicate code into bundle and instead applies a copy
     // of the function at runtime, which results smaller file size
