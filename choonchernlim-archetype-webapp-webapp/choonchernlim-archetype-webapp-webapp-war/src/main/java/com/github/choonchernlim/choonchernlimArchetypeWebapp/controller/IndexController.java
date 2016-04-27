7ea0d78b69c07df4c67b129d0069860b3ef7cfdd
Choon-Chern Lim <limc@mayo.edu>
2016-04-27 15:26:27 -0500

    Handled request URIs for single page app

--- choonchernlim-archetype-webapp-webapp/choonchernlim-archetype-webapp-webapp-war/src/main/java/com/github/choonchernlim/choonchernlimArchetypeWebapp/controller/IndexController.java
+++ choonchernlim-archetype-webapp-webapp/choonchernlim-archetype-webapp-webapp-war/src/main/java/com/github/choonchernlim/choonchernlimArchetypeWebapp/controller/IndexController.java
@@ -1,11 +1,17 @@
 package com.github.choonchernlim.choonchernlimArchetypeWebapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-// TODO configure a catch all (minus /api/*) to redirect user to index.html if user bookmark the single-page app links.
-//@Controller
-//@RequestMapping(value = "/")
+/**
+ * This controller catches all request URIs and presents the single-page app's entry page so that
+ * client side can figure out what to do with the request URI.
+ */
+// TODO LIMC push `index.html` to `/webapp/WEB-INF/html` instead of `/webapp`
+// TODO LIMC need to figure out where to put `favicon.png` to `assets/` dir
+@Controller
+@RequestMapping(value = "*")
 public final class IndexController {
     @RequestMapping(method = RequestMethod.GET)
     public String main() {
