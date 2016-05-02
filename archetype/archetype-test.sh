4d7ad2327786579b603450a73186661a991aad32
Choon-Chern Lim <limc@mayo.edu>
2016-05-02 15:40:38 -0500

    Fixed scripts

--- archetype/archetype-test.sh
+++ archetype/archetype-test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+echo "Using Java 1.7..."
 export JAVA_HOME="`/usr/libexec/java_home -v '1.7*'`"
 
-rm -rf /tmp/archetype/test-archetype
+echo "Removing /tmp/archetype/testProject..."
+rm -rf /tmp/archetype/testProject
 
+echo "Navigating to /tmp/archetype..."
 cd /tmp/archetype
 
-mvn archetype:generate -DinteractiveMode=false \
+echo "Generating project from archetype..."
+mvn archetype:generate -DinteractiveMode=false -DarchetypeCatalog=local \
 -DarchetypeGroupId=com.github.choonchernlim -DarchetypeArtifactId=choonchernlim-archetype-webapp -DarchetypeVersion=1.0.0-SNAPSHOT \
 -DgroupId=com.github.choonchernlim.testProject -DartifactId=testProject -Dversion=1.0.0-SNAPSHOT
